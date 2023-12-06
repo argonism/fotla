02c8b25128168a279b910944eff9ac738ca4f074
kush <kouta528bado@yahoo.co.jp>
2023-12-06 23:11:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,56 +1,3 @@
-# import os
-
-# from fotla.backend.corpus_loader import AdhocCorpusLoader
-# from fotla.backend.encoder import HFSymetricDenseEncoder
-# from fotla.backend.indexer.elasticsearch import (
-#     ElasticsearchBM25,
-#     ElasticsearchConfig,
-#     ElasticsearchIndexer,
-# )
-# from fotla.backend.retriever import DenseRetriever
-
-
-# def main():
-#     es_host = os.environ.get("ELASTICSEARCH_HOST", "localhost")
-#     es_port = os.environ.get("ELASTICSEARCH_PORT", 9200)
-#     es_config = ElasticsearchConfig(es_host, es_port)
-#     indexer = ElasticsearchIndexer(es_config, recreate_index=False)
-
-#     # encoder = HFSymetricDenseEncoder("facebook/mcontriever-msmarco")
-#     # retriever = DenseRetriever(encoder, indexer)
-#     retriever = ElasticsearchBM25(indexer)
-
-#     corpus_loader = AdhocCorpusLoader(
-#         [
-#             {
-#                 "doc_id": "1",
-#                 "text": "hello! This is the first doc.",
-#                 "title": "first doc",
-#             },
-#             {
-#                 "doc_id": "2",
-#                 "text": "world! This is the second doc.",
-#                 "title": "second doc",
-#             },
-#             {
-#                 "doc_id": "3",
-#                 "text": "hello world! This is the third doc.",
-#                 "title": "third doc",
-#             },
-#             {"doc_id": "3", "text": "This is the forth doc.", "title": "forth doc"},
-#         ]
-#     )
-#     retriever.index(corpus_loader)
-#     # start_api(retriever, port=9999)
-
-#     results = retriever.retrieve(["hello world"], 10)
-#     print(results)
-
-
-# if __name__ == '__main__':
-#     main()
-
-
 import os
 
 from fotla.backend.api import start_api
@@ -71,17 +18,17 @@ def load_indexer(recreate_index: bool = False):
     es_config = ElasticsearchConfig(
         es_host,
         es_port,
-        index_name="esa_contriever",
-        index_scheme_path=project_dir / "densa/mappings.json",
+        index_name="fotla",
+        index_scheme_path=project_dir / "vector_indexer/elasticsearch/mappngs.json",
     )
     indexer = ElasticsearchIndexer(es_config, recreate_index=recreate_index)
     return indexer
 
 
 def load_retirever(indexer):
-    encoder = HFSymetricDenseEncoder("facebook/mcontriever-msmarco")
-    retriever = DenseRetriever(encoder, indexer)
-    # retriever = ElasticsearchBM25(indexer)
+    # encoder = HFSymetricDenseEncoder("facebook/mcontriever-msmarco")
+    # retriever = DenseRetriever(encoder, indexer)
+    retriever = ElasticsearchBM25(indexer)
 
     return retriever
 
@@ -107,8 +54,8 @@ def index(retriever):
             {"doc_id": "3", "text": "This is the forth doc.", "title": "forth doc"},
         ]
     )
-    # retriever.index(corpus_loader)
-    retriever.async_index(corpus_loader)
+    retriever.index(corpus_loader)
+    # retriever.async_index(corpus_loader)
 
 
 def main(args):
